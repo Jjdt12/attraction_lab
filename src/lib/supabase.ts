be61e3d3bda5784aef3b2abff48d82d66a168a12
Tully <140899832+Jjdt12@users.noreply.github.com>
2025-11-07 11:54:33 -0500

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://sydmziedhwcsemujevwu.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5ZG16aWVkaHdjc2VtdWpldnd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxOTQ0OTgsImV4cCI6MjA3Nzc3MDQ5OH0.iMqtMcBG2l4fCEFri-HNs9xUajDP5knmJ3g_z2sBKIE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
